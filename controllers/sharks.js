b80f41e49dfdd276ac4674c815233761976d5745
Victors1967 <victorsmirnov@me.com>
2021-10-16 13:26:48 +0300

    add functions

--- controllers/sharks.js
+++ controllers/sharks.js
@@ -4,3 +4,14 @@ const Shark = require('../models/sharks');
 
 exports.index = (req, res) => res.sendFile(path.resolve('views/sharks.html'));
 
+exports.create = (req, res) => {
+  let newShark = new Shark(req.body);
+  console.log(req.body);
+  newShark.save(err =>
+    err ? res.status(400).send('Unable to save shark to database.') : res.redirect('shark/getshark'));
+};
+
+exports.list = (req, res) => {
+  Shark.find({}).exec((err, sharks) =>
+    err ? res.send(500, err) : res.render('getshark', { sharks: sharks }));
+};
